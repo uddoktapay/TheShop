ffbf01ce998d348d6ac7db0ea26407a66203e1da
Rasel Islam Rafi <31556372+rtraselbd@users.noreply.github.com>
2025-10-18 08:24:10 +0600

    Update uddoktapay.php

--- config/uddoktapay.php
+++ config/uddoktapay.php
@@ -3,4 +3,5 @@
 return [
     'api_key' => env('UDDOKTAPAY_API_KEY', '982d381360a69d419689740d9f2e26ce36fb7a50'),
     'api_url' => env('UDDOKTAPAY_API_URL', 'https://sandbox.uddoktapay.com/api'),
+    'display_name' => env('UDDOKTAPAY_DISPLAY_NAME', 'BD Payment Gateway')
 ];
